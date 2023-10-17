5f2fc5caf5a22c0ad0e189bd4ec0a6d292457768
Momin Iqbal <61788052+MominIqbal-1234@users.noreply.github.com>
2023-10-18 01:12:34 +0500

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -3,12 +3,12 @@ from cryptography.fernet import Fernet
 
 class hide:
     def encode_message(message1):
-        fernet = Fernet(b'K4UptMTVmGCYMzeV_MA0TZIQ52FgPOnoyvMkJ0bPF3w=')
+        fernet = Fernet(b'my_key')
         encMessage = fernet.encrypt(message1.encode())
         return encMessage
 
     def decode_message(message1):
         message1 = bytes(message1,'utf-8')
-        fernet = Fernet(b'K4UptMTVmGCYMzeV_MA0TZIQ52FgPOnoyvMkJ0bPF3w=')
+        fernet = Fernet(b'my_key')
         decMessage = fernet.decrypt(message1).decode()
         return decMessage
