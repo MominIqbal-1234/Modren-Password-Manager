82f06a660a7c5c4a54ad449cbc7d9eaf0dbd8972
Momin <mominiqbal1234@gmail.com>
2023-11-19 21:28:02 +0500

    fix bug

---
+++ generate_key.py
@@ -0,0 +1,2 @@
+from webraft.core import GenerateKey
+print(GenerateKey.generate_key()) # Generate New Key
--- hidden.py
+++ hidden.py
@@ -3,12 +3,22 @@ from cryptography.fernet import Fernet
 
 class hide:
     def encode_message(message1):
-        fernet = Fernet(b'my_key')
+        """
+        Dont use in Production 
+        its Public Key
+        """
+        fernet = Fernet(b'FVv3JlQzbaZJom2IH9NNAy0cDJy_Hv-b2J5DxfWRsXE=')
         encMessage = fernet.encrypt(message1.encode())
         return encMessage
 
     def decode_message(message1):
+        """
+        Dont use in Production 
+        its Public Key
+        """
         message1 = bytes(message1,'utf-8')
-        fernet = Fernet(b'my_key')
+        fernet = Fernet(b'FVv3JlQzbaZJom2IH9NNAy0cDJy_Hv-b2J5DxfWRsXE=')
         decMessage = fernet.decrypt(message1).decode()
         return decMessage
+
+
